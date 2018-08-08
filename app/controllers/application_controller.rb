225e014546ccf3aa355c8e51958d6b9940a44245
github username <github email address>
2018-08-08 07:43:17 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,95 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
+
+  get '/recipes/new' do #loads new form
+    erb :new
+  end
+
+  get '/recipes' do #loads index page
+    @recipes = Recipe.all
     erb :index
   end
+
+  get '/recipes/:id' do  #loads show page
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do #loads edit form
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do  #updates a recipe
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  post '/recipes' do  #creates a recipe
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id/delete' do #delete action
+  @recipe = Recipe.find_by_id(params[:id])
+  @recipe.delete
+  redirect to '/recipes'
 end
+
+
+end
+# class ApplicationController < Sinatra::Base
+#   configure do
+#     set :public_folder, 'public'
+#     set :views, 'app/views'
+#   end
+#   #why does this method needs to be first?
+#   #re-watch lecture
+#   get '/recipes/new' do
+#     erb :new
+#   end
+#
+#   get '/recipes' do
+#     @recipes = Recipe.all
+#     erb :index
+#   end
+#
+#   get '/recipes/:id' do
+#     @recipe = Recipe.find(params[:id])
+#
+#     erb :show
+#   end
+#
+#   get '/recipes/:id/edit' do
+#     @recipe = Recipe.find(params[:id])
+#
+#     erb :edit
+#   end
+#
+#   patch '/recipes/:id' do
+#     @recipe = Recipe.find_by_id(params[:id])
+#     @recipe.name = params[:name]
+#     @recipe.ingredients = params[:ingredients]
+#     @recipe.cook_time = params[:cook_time]
+#     @recipe.save
+#     redirect "/recipes/#{@recipe.id}"
+#   end
+#
+#   post '/recipes/:id' do
+#     @recipe = Recipe.create(params)
+#     redirect "/recipes/#{@recipe.id}"
+#   end
+#
+#
+#
+#   delete '/recipes/:id/delete' do
+#     Recipe.destory(params[:id])
+#     redirect to '/recipes'
+#   end
+#
+# end
